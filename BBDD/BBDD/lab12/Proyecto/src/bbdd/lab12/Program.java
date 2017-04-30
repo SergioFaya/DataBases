af77a6a7b3b5c287c26ee2e0a341735a74f1884c
Sergio <estisergio@gmail.com>
2017-04-30 11:35:11 +0200

    Lab 12 update

--- BBDD/BBDD/lab12/Proyecto/src/bbdd/lab12/Program.java
+++ BBDD/BBDD/lab12/Proyecto/src/bbdd/lab12/Program.java
@@ -15,8 +15,8 @@ public class Program {
 //		System.out.println("Leer una cadena por teclado");	
 //		String cadena = ReadString();
 		con = DriverManager.getConnection(URL_HYPERSONIC, "SA","");
-		exercise1_1();
-		exercise1_2();
+//		exercise1_1();
+//		exercise1_2();
 		exercise2();
 	
 		
@@ -25,7 +25,7 @@ public class Program {
 	}
 
 	/*
-		1. Mostrar por pantalla los resultados de las consultas 21 y 32 de la Práctica 2. 
+		1. Mostrar por pantalla los resultados de las consultas 21 y 32 de la Prï¿½ctica 2. 
 		1.1. 21. Obtener el nombre y el apellido de los clientes que han adquirido un coche en un concesionario de Madrid, el cual dispone de coches del modelo gti.
 	 */
 	public static void exercise1_1() throws SQLException {
@@ -75,14 +75,14 @@ public class Program {
 	}
 	
 	/*
-		2. Mostrar por pantalla el resultado de la consulta 6 de la Práctica 2 de forma el color de la búsqueda sea introducido por el usuario.
+		2. Mostrar por pantalla el resultado de la consulta 6 de la Prï¿½ctica 2 de forma el color de la bï¿½squeda sea introducido por el usuario.
 			6. Obtener el nombre de las marcas de las que se han vendido coches de un color introducido por el usuario.
 	*/
 	public static void exercise2() throws SQLException {
 		System.out.println("Introduce a color");
 		String color = ReadString();
-		ResultSet rs3 = st.executeQuery("select  nombrem FROM marcas m,coches c,marco mc ,ventas v "
-				+ "where m.cifm = mc.cifm and mc.CODCOCHE = c.CODCOCHE select apellido from clientes where ciudad ='madrid' "
+		ResultSet rs3 = st.executeQuery("select nombrem FROM marcas m,coches c,marco mc ,ventas v "
+				+ "where m.cifm = mc.cifm and mc.CODCOCHE = c.CODCOCHE "
 				+ "and c.CODCOCHE = v.codcoche and v.color = '"
 				+ color
 				+ "'");
@@ -100,7 +100,7 @@ public class Program {
 	}
 	
 	/*
-		3. Realizar una aplicación en Java para ejecutar la consulta 27 de la Práctica 2 de forma que los límites la cantidad de coches sean introducidos por el usuario. 
+		3. Realizar una aplicaciï¿½n en Java para ejecutar la consulta 27 de la Prï¿½ctica 2 de forma que los lï¿½mites la cantidad de coches sean introducidos por el usuario. 
 			27. Obtener el cifc de los concesionarios que disponen de una cantidad de coches comprendida entre dos cantidades introducidas por el usuario, ambas inclusive.
 	 */
 	public static void exercise3() {
@@ -108,7 +108,7 @@ public class Program {
 	}
 	
 	/*
-		4. Realizar una aplicación en Java para ejecutar la consulta 24 de la Práctica 2 de forma que tanto la ciudad del concesionario como el color sean introducidos por el usuario. 
+		4. Realizar una aplicaciï¿½n en Java para ejecutar la consulta 24 de la Prï¿½ctica 2 de forma que tanto la ciudad del concesionario como el color sean introducidos por el usuario. 
 			24.	Obtener los nombres de los clientes que no han comprado coches de un color introducido por el usuario en concesionarios de una ciudad introducida por el usuario.
 	 */
 	public static void exercise4() {
@@ -116,7 +116,7 @@ public class Program {
 	}
 	
 	/*
-		5. Realizar una aplicación en Java que haciendo uso de la instrucción SQL adecuada: 
+		5. Realizar una aplicaciï¿½n en Java que haciendo uso de la instrucciï¿½n SQL adecuada: 
 		5.1. Introduzca datos en la tabla coches cuyos datos son introducidos por el usuario
 	 */
 	public static void exercise5_1() {
@@ -124,22 +124,22 @@ public class Program {
 	}
 	
 	/*
-		5.2. Borre un determinado coche cuyo código es introducido por el usuario. 
+		5.2. Borre un determinado coche cuyo cï¿½digo es introducido por el usuario. 
 	 */
 	public static void exercise5_2() {
 		
 	}
 	
 	/*	 
-		5.3. Actualice el nombre y el modelo para un determinado coche cuyo código es introducido por el usuario.
+		5.3. Actualice el nombre y el modelo para un determinado coche cuyo cï¿½digo es introducido por el usuario.
 	 */
 	public static void exercise5_3() {		
 		
 	}
 	
 	/*
-		6. Invocar la función y el procedimiento del ejercicio 10 de la Práctica 10 desde una aplicación Java. 
-			10.	Realizar un procedimiento y una función que dado un código de concesionario devuelve el número ventas que se han realizado en el mismo.
+		6. Invocar la funciï¿½n y el procedimiento del ejercicio 10 de la Prï¿½ctica 10 desde una aplicaciï¿½n Java. 
+			10.	Realizar un procedimiento y una funciï¿½n que dado un cï¿½digo de concesionario devuelve el nï¿½mero ventas que se han realizado en el mismo.
 		6.1. Funcion
 	 */
 	public static void exercise6_1() {		
@@ -154,8 +154,8 @@ public class Program {
 	}
 	
 	/*
-		7. Invocar la función y el procedimiento del ejercicio 11 de la Práctica 10 desde una aplicación Java. 
-			11. Realizar un procedimiento y una función que dada una ciudad que se le pasa como parámetro devuelve el número de clientes de dicha ciudad.
+		7. Invocar la funciï¿½n y el procedimiento del ejercicio 11 de la Prï¿½ctica 10 desde una aplicaciï¿½n Java. 
+			11. Realizar un procedimiento y una funciï¿½n que dada una ciudad que se le pasa como parï¿½metro devuelve el nï¿½mero de clientes de dicha ciudad.
 		7.1. Funcion
 	 */
 	public static void exercise7_1() {		
